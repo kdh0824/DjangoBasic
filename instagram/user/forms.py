10312b245a3f721cb5111b6b6ca30c0479d140c2
donghyeon kang <kdh0824_@naver.com>
2021-10-04 21:14:01 +0900

    회원 가입 기능 추가 - 패스워드, 이메일 암호화 X

---
+++ instagram/user/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from .models import User
+from django.contrib.auth.hashers import check_password
+
+
--- instagram/user/views.py
+++ instagram/user/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 
+from .models import User
+
+
+# from .forms import RegisterForm
+
 
 # Create your views here.
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        userid = request.POST.get('id', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
+        email = request.POST.get('email', None)
+        mobile = request.POST.get('mobile', None)
+        username = request.POST.get('username', None)
+
+        res_data = {}
+
+        if not (userid and password and re_password and email and mobile and username):
+            res_data['error'] = '비어있는 필드가 있습니다.'
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            user = User(
+                id=userid,
+                password=password,
+                email=email,
+                mobile=mobile,
+                username=username
+            )
+
+            user.save()
+        return render(request, 'register.html', res_data)
